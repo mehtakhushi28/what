c58789045cfff7a8475b6d2caef2324da4206cd1
mehtakhushi28 <mehtakhushi444@gmail.com>
2024-09-19 19:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ option2.addEventListener("click", function() {
     nextScreen.style.display = "block"; // Show the next screen
 
     // Play the background music
-    backgroundMusic.play().catch(error => {
+    careless.play().catch(error => {
         console.error("Error playing audio:", error);
     });
 
